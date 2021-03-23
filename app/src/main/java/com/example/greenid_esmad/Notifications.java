0a710bc5ca4ae8c67c909c5a8ca754d81a2e4e8e
Claudia <claudianamiko@hotmail.com>
2021-03-23 17:05:12 +0000

    RecyclerView Notifications

--- app/src/main/java/com/example/greenid_esmad/Notifications.java
+++ app/src/main/java/com/example/greenid_esmad/Notifications.java
@@ -2,10 +2,12 @@ package com.example.greenid_esmad;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -17,9 +19,20 @@ import com.example.greenid_esmad.NewPost;
 import com.example.greenid_esmad.R;
 import com.example.greenid_esmad.Search;
 import com.example.greenid_esmad.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class Notifications extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    NotificationsAdapter notificationsAdapter;
+    ArrayList<ContentNotifications> contentNotifications = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +81,44 @@ public class Notifications extends AppCompatActivity {
                 return false;
             }
         });
-    }
+        //Access user Id from GLOBALS
+        GLOBALS globalUserId = (GLOBALS) getApplicationContext();
+        String userId = globalUserId.getUserIdGlobal();
+        Log.d("USERID", userId);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("users").document(userId).collection("notifications")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("USER POSTS", document.getId() + " => " + document.getData());
+
+                                String authorPfp = document.getString("pfpUrl");
+                                String contentUrl = document.getString("contentUrl");
+                                String commentVal = document.getString("commentVal");
+                                String author = document.getString("username");
+
+                                contentNotifications.add(new ContentNotifications (authorPfp, contentUrl, commentVal, author));
+
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+
+                    }
+                });
+    }
+    private void RecyclerCall() {
+
+        recyclerView = findViewById(R.id.notificationrv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        notificationsAdapter = new NotificationsAdapter(this, contentNotifications);
+        recyclerView.setAdapter(notificationsAdapter);
+
+    }
 }
---
+++ app/src/main/java/com/example/greenid_esmad/NotificationsAdapter.java
@@ -0,0 +1,101 @@
+package com.example.greenid_esmad;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class NotificationsAdapter extends RecyclerView.Adapter<NotificationsAdapter.ViewHolder>{
+    //criação de referencias
+    private List<ContentNotifications> mData;
+    private LayoutInflater mInflater;
+    private NotificationsAdapter.ItemClickListener mClickListener;
+
+    NotificationsAdapter(Context context, List<ContentNotifications> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @Override
+    public  NotificationsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.notification_card, parent, false);
+        return new NotificationsAdapter.ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+
+        ContentNotifications contentNotifications = mData.get(position);
+        holder.tvAuthor.setText(contentNotifications.getAuthor());
+        holder.tvCommentVal.setText(contentNotifications.getCommentVal());
+
+
+        final String authorPfp = contentNotifications.getAuthorPfp();
+        final String contentUrl = contentNotifications.getContentUrl();
+
+
+        Picasso.get().load(authorPfp).into(holder.profile_image);
+        Picasso.get().load(contentUrl).into(holder.contentPic);
+
+
+        holder.resultCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TAG", "XPTO");
+
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvAuthor;
+        TextView tvCommentVal;
+        ImageView profile_image;
+        ImageView contentPic;
+        RelativeLayout resultCard;
+
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            tvAuthor = itemView.findViewById(R.id.pf_name);
+            tvCommentVal = itemView.findViewById(R.id.comments_val);
+            contentPic = itemView.findViewById(R.id.post_image);
+            profile_image = itemView.findViewById(R.id.profile_image);
+            resultCard = itemView.findViewById(R.id.post_card);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }}
