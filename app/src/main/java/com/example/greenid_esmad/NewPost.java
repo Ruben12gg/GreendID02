79e3b7a69c5b9c28813dd626853d9a0de103331f
Ruben <40190328@esmad.ipp.pt>
2021-03-29 22:43:09 +0100

    Image Post Upload
    (TODO: generate full post activity do firebase)

--- app/src/main/java/com/example/greenid_esmad/NewPost.java
+++ app/src/main/java/com/example/greenid_esmad/NewPost.java
@@ -1,20 +1,58 @@
 package com.example.greenid_esmad;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.net.URI;
+import java.util.UUID;
 
 public class NewPost extends AppCompatActivity {
+
+    ImageView contentPic;
+    Uri imageUri;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    ImageView contentPic2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        contentPic = findViewById(R.id.imageContent);
+
+        //Click on the imageView
+        contentPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+            }
+        });
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -26,16 +64,16 @@ public class NewPost extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                switch(menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
 
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext(), Home.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.search:
                         startActivity(new Intent(getApplicationContext(), Search.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.new_post:
@@ -43,13 +81,13 @@ public class NewPost extends AppCompatActivity {
 
                     case R.id.user:
                         startActivity(new Intent(getApplicationContext(), User.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
 
 
                     case R.id.notifications:
                         startActivity(new Intent(getApplicationContext(), Notifications.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
 
 
@@ -58,5 +96,87 @@ public class NewPost extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    //Function to choose pic
+    private void choosePicture() {
+
+        //Launch intent with the gallery viewer
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    //When image is selected from gallery
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //if the file exists and is successfully 'obtained'
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+
+            contentPic.setImageURI(imageUri);
+
+            uploadPicture();
+        }
+    }
+
+    //Upload the previously selected pic to database
+    private void uploadPicture() {
+
+        //create and show a progress upload bar
+        final ProgressDialog progDiag = new ProgressDialog(this);
+        progDiag.setTitle("Uploading Image...");
+        progDiag.show();
+
+        //Generate a random name for the file
+        final String randomKey = UUID.randomUUID().toString();
+        StorageReference imageRef = storageReference.child("images/" + randomKey);
+
+        //upload the file to the database and generate an usable url to display the picture
+        imageRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get an URL to the uploaded content
+
+                        //Generate img link
+                        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+
+                                contentPic2 = findViewById(R.id.imageContent2);
+
+                                String downloadUrl3 = uri.toString();
+                                Log.d("IMAGE POST LINK", downloadUrl3);
+
+                                Picasso.get().load(downloadUrl3).into(contentPic2);
+
+                            }
+                        });
+
+                        progDiag.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content), "Image Uploaded!", Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        progDiag.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed to Upload File", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            //Progress bar percentage calculation
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progressPercentage = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+
+                progDiag.setMessage("Loading " + (int) progressPercentage + "%");
+            }
+        });
+
     }
 }
