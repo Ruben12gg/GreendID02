1a7c18813675f8d97bc0a41073544e4e40e60151
Luís <henriqueluis01@outlook.com>
2021-04-15 16:15:14 +0100

    Comments activity, card and start logic

---
+++ app/src/main/java/com/example/greenid_esmad/Comments.java
@@ -0,0 +1,101 @@
+package com.example.greenid_esmad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class Comments extends AppCompatActivity {
+
+    EditText add_comment;
+    ImageView pfp;
+    TextView post;
+
+    String postid;
+    String publisherid;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Comments");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        add_comment = findViewById(R.id.add_comment);
+        pfp = findViewById(R.id.pfp);
+        post = findViewById(R.id.post);
+
+        Intent intent = getIntent();
+        postid = intent.getStringExtra("postid");
+        publisherid = intent.getStringExtra("publisherid");
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (add_comment.getText().toString().equals("")){
+                    Toast.makeText(Comments.this, "The comment cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    add_comment();
+                }
+            }
+        });
+
+        // getImage();
+    }
+
+    private void add_comment(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Comments").child(postid);
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("comment", add_comment.getText().toString());
+        hashMap.put("publisher", firebaseUser.getUid());
+
+        reference.push().setValue(hashMap);
+        add_comment.setText("");
+    }
+
+    /* private void getImage(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addChildEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Glide.with(getApplicationContext()).load(user.getImageurl).into(pfp);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    } */
+}
