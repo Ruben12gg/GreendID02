58e5a41c6bedb23ba34fb1d60bdfe0a7fd525da6
Ruben <40190328@esmad.ipp.pt>
2021-04-25 23:20:25 +0100

    Removed useless code

--- app/src/main/java/com/example/greenid_esmad/Settings.java
+++ app/src/main/java/com/example/greenid_esmad/Settings.java
@@ -139,12 +139,10 @@ public class Settings extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), User.class));
             }
         });
-
-
+        
 
     }
 
-    //This will be useful do delete app cache directly from app
 
     private void logout() {
         GoogleSignInClient mGoogleSignInClient;
@@ -166,30 +164,5 @@ public class Settings extends AppCompatActivity {
                     }
                 });
     }
-
-
-   /* public static class Modal extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage("Bruh")
-                    .setPositiveButton("bruh!", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // FIRE ZE MISSILES!
-                        }
-                    })
-                    .setNegativeButton("No bruh", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            return builder.create();
-        }
-
-
-    }*/
-
 
 }
