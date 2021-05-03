641cd3cbbe7e12deb120e88a7795a462701b49ee
Ruben <40190328@esmad.ipp.pt>
2021-05-03 21:06:41 +0100

    GLOBALS clean up

--- app/src/main/java/com/example/greenid_esmad/GLOBALS.java
+++ app/src/main/java/com/example/greenid_esmad/GLOBALS.java
@@ -5,26 +5,6 @@ import android.app.Application;
 public class GLOBALS extends Application {
 
     private String userIdGlobal;
-    private String userName;
-
-    public String getUserPfp() {
-        return userPfp;
-    }
-
-    public void setUserPfp(String userPfp) {
-        this.userPfp = userPfp;
-    }
-
-    private String userPfp;
-
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 
     public String getUserIdGlobal() {
         return userIdGlobal;
@@ -34,6 +14,5 @@ public class GLOBALS extends Application {
         this.userIdGlobal = userIdGlobal;
     }
 
-    public void setUserIdG(String userId) {
-    }
+
 }
